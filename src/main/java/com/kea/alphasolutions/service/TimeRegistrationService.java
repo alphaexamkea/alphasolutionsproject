9b2eaee20e1c7ade8e2803851c07db5ba71611a2
christianvinther <christian@vinther.dk>
2025-08-22 12:42:25 +0200

    Task/Timereg
    Automatic. TODO->Progress when time is logged on task

--- src/main/java/com/kea/alphasolutions/service/TimeRegistrationService.java
+++ src/main/java/com/kea/alphasolutions/service/TimeRegistrationService.java
@@ -1,5 +1,6 @@
 package com.kea.alphasolutions.service;
 
+import com.kea.alphasolutions.model.Task;
 import com.kea.alphasolutions.model.TimeRegistration;
 import com.kea.alphasolutions.repository.TimeRegistrationRepository;
 import org.springframework.stereotype.Service;
@@ -10,9 +11,11 @@ import java.util.List;
 public class TimeRegistrationService {
 
     private final TimeRegistrationRepository timeRegistrationRepository;
+    private final TaskService taskService;
 
-    public TimeRegistrationService(TimeRegistrationRepository timeRegistrationRepository) {
+    public TimeRegistrationService(TimeRegistrationRepository timeRegistrationRepository, TaskService taskService) {
         this.timeRegistrationRepository = timeRegistrationRepository;
+        this.taskService = taskService;
     }
 
     public List<TimeRegistration> getTimeRegistrationsByTaskId(int taskId) {
@@ -29,6 +32,7 @@ public class TimeRegistrationService {
 
     public void addTimeRegistration(TimeRegistration timeRegistration) {
         timeRegistrationRepository.save(timeRegistration);
+        autoUpdateTaskStatus(timeRegistration.getTaskId());
     }
 
     public void updateTimeRegistration(TimeRegistration timeRegistration) {
@@ -49,5 +53,13 @@ public class TimeRegistrationService {
 
     public double getTotalHoursByProjectId(int projectId) {
         return timeRegistrationRepository.getTotalHoursByProjectId(projectId);
+    }
+
+    private void autoUpdateTaskStatus(int taskId) {
+        Task task = taskService.getTaskById(taskId);
+        if (task != null && "TO_DO".equals(task.getStatus())) {
+            task.setStatus("IN_PROGRESS");
+            taskService.updateTask(task);
+        }
     }
 }
